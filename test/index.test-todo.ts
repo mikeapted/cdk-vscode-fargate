2329db07df1f28459a877436667dfb5d401e95bd
Mike Apted <mikapted@amazon.com>
2021-05-16 17:02:23 -0400

    feat: initial construct

---
+++ test/index.test-todo.ts
@@ -0,0 +1,19 @@
+// import * as cdk from '@aws-cdk/core';
+// import { VSCodeFargate } from '../src/index';
+
+// import '@aws-cdk/assert/jest';
+
+// const env = {
+//   region: process.env.CDK_DEFAULT_REGION,
+//   account: process.env.CDK_DEFAULT_ACCOUNT,
+// };
+
+// const subdomain = process.env.VSCODE_SUBDOMAIN ?? 'vscode';
+// const domainName = process.env.VSCODE_DOMAIN_NAME ?? 'mydomain.com';
+
+// test('create app', () => {
+//   const app = new cdk.App();
+//   const stack = new cdk.Stack(app, 'TestStack', { env });
+//   new VSCodeFargate(stack, 'TestConstruct', { subdomain, domainName });
+//   expect(stack).toHaveResource('AWS::ECS::Cluster');
+// });
