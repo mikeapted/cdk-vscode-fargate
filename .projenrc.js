9cab3a26a04a29350fcfc454bcf221927ccacad1
Mike Apted <mikapted@amazon.com>
2021-05-20 14:07:32 -0400

    CDK version bump to 1.105.0

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,7 @@ const { AwsCdkConstructLibrary } = require('projen');
 const project = new AwsCdkConstructLibrary({
   author: 'Mike Apted',
   authorAddress: 'mike.apted@gmail.com',
-  cdkVersion: '1.104.0',
+  cdkVersion: '1.105.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-vscode-fargate',
   repositoryUrl: 'https://github.com/mikeapted/cdk-vscode-fargate.git',
